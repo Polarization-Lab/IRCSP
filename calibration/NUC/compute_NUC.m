f1c3efc8a83663a7ae530a1dfdf262f55342a7e0
Kira Hart <khart@optics.arizona.edu>
2020-10-07 13:56:12 -0700

    final commit for inlab work oct. 7

--- calibration/NUC/compute_NUC.m
+++ calibration/NUC/compute_NUC.m
@@ -1,8 +1,9 @@
 %% Import data
 fn = "test3.h5";
-[cam1,cam2,t1,t2] = load_NUCdata(fn);
+[rcam1,rcam2,t1,t2] = load_NUCdata(fn);
 
 %% Plot Temperatures 
+subplot(2,1,1)
 hold on
 plot(t1,'.-')
 plot(t2,'.-')
@@ -11,25 +12,31 @@ xlabel('measurement number')
 hold off
 legend('Camera 1','Camera 2')
 
-%% Select which data points to use for each camera
-rcam1 = cam1(1:4,:,:);
-rcam2 = cam1(1:4,:,:);
+subplot(2,1,2)
+hold on
+plot(squeeze(cam1(120,100,:)),'.-')
+plot(squeeze(cam2(120,100,:)),'.-')
+ylabel('avg. FPA value')
+xlabel('measurement number')
+hold off
+legend('Camera 1','Camera 2')
 
-t1s = t1(1:4);
-t2s = t2(1:4);
+%% Select which data points to use for each camera
+
+
 
 %% Select ROI and input to NUC function
 %cam 1
-r112 = rcam1(1,:,:)-rcam1(2,:,:);
-r134 = rcam1(3,:,:)-rcam1(4,:,:);
-t112 = t1s(1) - t1s(2);
-t134 = t1s(3) - t1s(4);
+r112 = rcam1(:,:,1)-rcam1(:,:,5);
+r134 = rcam1(:,:,6)-rcam1(:,:,10);
+t112 = t1(1) - t1(5);
+t134 = t1(6) - t1(10);
 
 
 %cam 2
-r212 = rcam2(1,:,:)-rcam2(2,:,:);
-r212 = rcam2(3,:,:)-rcam2(4,:,:);
-t212 = t2s(1) - t2s(2);
-t234 = t2s(3) - t2s(4);
+r212 = rcam2(:,:,1)-rcam2(:,:,5);
+r212 = rcam2(:,:,6)-rcam2(:,:,10);
+t212 = t2(1) - t2(5);
+t234 = t2(6) - t2(10);
 
 %% Save Computed
