e3ea7da7fc49ee48ae669e99780e88277ce9760c
Kira Hart <khart@optics.arizona.edu>
2020-10-07 13:25:58 -0700

    updating compute_NUC

--- calibration/NUC/compute_NUC.m
+++ calibration/NUC/compute_NUC.m
@@ -12,13 +12,24 @@ hold off
 legend('Camera 1','Camera 2')
 
 %% Select which data points to use for each camera
-cam1_points = [3,4,5];
-cam2_points = [1,3,4];
+rcam1 = cam1(1:4,:,:);
+rcam2 = cam1(1:4,:,:);
 
-
-rcam1 = cam1(3:5,:,:);
-rcam2 = cam1(13:5,:,:)
+t1s = t1(1:4);
+t2s = t2(1:4);
 
 %% Select ROI and input to NUC function
+%cam 1
+r112 = rcam1(1,:,:)-rcam1(2,:,:);
+r134 = rcam1(3,:,:)-rcam1(4,:,:);
+t112 = t1s(1) - t1s(2);
+t134 = t1s(3) - t1s(4);
+
+
+%cam 2
+r212 = rcam2(1,:,:)-rcam2(2,:,:);
+r212 = rcam2(3,:,:)-rcam2(4,:,:);
+t212 = t2s(1) - t2s(2);
+t234 = t2s(3) - t2s(4);
 
 %% Save Computed
