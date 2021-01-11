6d2e90cb9ae9101a2a50714d6c8698173abf5783
kira-hart <khart@optics.arizona.edu>
2021-01-11 16:18:33 -0700

    generalized compute NUC coefficients from pandas

--- calibration/NUC/NUC_functions.py
+++ calibration/NUC/NUC_functions.py
@@ -81,3 +81,74 @@ def collapse_df(df):
         im = im/len(inds)
         images1.append(np.around(im,decimals =0))
     return(values,images1)    
+
+
+
+def compile_NUC_matrix_input(df):
+    '''
+    this function will prepare NUC averaged data for NUC_coef()
+    df should be averages df from collapse df
+    returns and determines reference  temperature 
+    returns arrays T adn r for NUC_coef()
+    '''
+    #find FPA temp with most instances, this is the reference temp
+    T_ref = (df['temps1'].value_counts().index.tolist())[0]
+
+    #find BB temps which have a FPA measurement at T_ref
+    bbs = np.array(df.query('temps1=='+str(T_ref))['bbtemp'])
+
+    #find unique temps that have more than one FPA temp
+    Ts =np.array(df.bbtemp.value_counts().loc[lambda x: x>1].index)
+
+    #find common list of BB temps with at least 2 members and T_ref
+    ts = np.intersect1d(Ts,bbs)
+
+    #make new df
+    for i in range(len(ts)):
+        dfs = df.loc[df['bbtemp'] == ts[i]]
+
+        #select T-ref first, then other temp 
+        T1 = np.array(dfs.loc[dfs['temps1'] == T_ref]['temps1'])[0]
+        T2 = np.array(dfs.loc[dfs['temps1'] != T_ref]['temps1'])[0]
+        r1 = np.array(dfs.loc[dfs['temps1'] == T_ref]['images1'])[0]
+        r2 = np.array(dfs.loc[dfs['temps1'] != T_ref]['images1'])[0]
+        tfpas = [T1,T2]
+        responses = [r1,r2]
+        if i == 0:
+            tFPAS = tfpas;
+            rs = responses
+        else:
+            tFPAS = np.concatenate((tFPAS, tfpas), axis=0)
+            rs = np.concatenate((rs, responses), axis=0)
+        
+    return(T_ref, tFPAS, rs)       
+
+
+
+def calc_NUC_coef(tFPAS,rs,x,y):
+    '''this function calculates the cal. coef for a given pizzel
+    takes input calculated using  compile_NUC_matrix_input''' 
+    i = 0;
+    matrix = [];
+    delta_r = [];
+    while i <len(tFPAS):
+        T1 = tFPAS[i]
+        T2 = tFPAS[i+1]
+        r1 = (rs[i])[y][x]
+        r2 = (rs[i+1])[y][x]
+        T12 = T1 - T2;
+        r12 = r1-r2
+
+        row = [r1*T12,T12]
+
+        matrix.append(row)
+        delta_r.append(r12)
+
+        i = i +2
+
+    #now compute psuedo inverse    
+    matinv = np.linalg.pinv(matrix)
+
+    #now compute m,b
+    [m , b ] = np.matmul(matinv, delta_r)
+    return(m,b)
